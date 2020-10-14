6e36c575eccc1fcd29dff6f43d7ca86b4a69d23d
Adam Surya Des <fourline66@gmail.com>
2020-10-14 16:22:49 +0700

    up mail to

--- resources/views/cms/page/inbox/read.blade.php
+++ resources/views/cms/page/inbox/read.blade.php
@@ -1,6 +1,9 @@
 <div class="card card-primary">
 	<div class="card-header">
 		<h3 class="card-title">Message From {{ $inbox->name }}</h3>
+        <div class="card-tools">
+            <a href="mailto:{{ $inbox->email }}?subject={{ urlencode($inbox->subject) }}&body={{ urlencode($inbox->message) }}">Send Mail</a>
+        </div>
 	</div>
     <div class="card-body">
         <div class="row">
