1900899af3db417850e8ead5529d7885d6132a00
Adam Surya Des <fourline66@gmail.com>
2020-10-19 17:16:20 +0700

    update footer

--- resources/views/frontend/layout/footer.blade.php
+++ resources/views/frontend/layout/footer.blade.php
@@ -52,6 +52,11 @@
           <div class="row pt-3 text-center">
             <div class="col-md-12">
               <div class="border-top pt-5">
+              <div class="text-center mb-2">
+                <a href="https://www.instagram.com/lazatto.id/" target="_blank"><img src="{{ asset('asset/picture/default/icon_ig.png') }}" alt="social media" width="40px"></a>
+                <a href="https://twitter.com/LazattoChicken?s=09" target="_blank"><img src="{{ asset('asset/picture/default/icon_tw.png') }}" alt="social media" width="40px"></a>
+                <a href="https://api.whatsapp.com/send?phone={{ App\Http\Controllers\HomeController::getSiteConfig()->config['wa_no'] }}&text=Terima%20kasih%20telah%20menghubungi%20info%20kemitraan%20Lazatto%20Chicken%20&%20Burger." target="_blank"><img src="{{ asset('asset/picture/default/icon_wa.png') }}" alt="social media" width="40px"></a>
+              </div>
               <p class="copyright">
                 &copy;<script>document.write(new Date().getFullYear());</script> Lazatto Chicken & Burger<br>
                 Web Development By <a href="https://f-plat.id" target="_blank"><strong>PT. Fortuna Platina Indonesia</strong></a>
